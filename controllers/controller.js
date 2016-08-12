7e3fabdb4e6d3c038190bd11c199895844ea4797
James Hornung <hornung.james@gmail.com>
2016-08-11 19:27:50 -0500

    updates

--- controllers/controller.js
+++ controllers/controller.js
@@ -168,7 +168,7 @@ router.post('/listings', function(req, res) {
         method: 'GET',
         url: 'https://ws.homeaway.com/public/search',
         qs: { 
-            // q: city,
+            q: city,
             minSleeps: sleeps, 
             // availabilityStart: yyyy-MM-dd,
             // availabilityEnd: yyy-MM-dd, 
